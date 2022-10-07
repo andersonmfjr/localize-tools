c4b4513d8a0baf3d4ef00276c06f53d9ea999196
Anderson Feitosa <andersonmfjr@gmail.com>
2022-10-07 16:14:23 -0300

    feat(home): setup initial logic to view empty state

--- src/app/views/home/components/projects-list/projects-list.component.ts
+++ src/app/views/home/components/projects-list/projects-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../../../shared/models/project.model';
+import { ProjectService } from '../../../../shared/services/project/project.service';
 
 @Component({
   selector: 'app-projects-list',
   templateUrl: './projects-list.component.html',
-  styleUrls: ['./projects-list.component.scss']
+  styleUrls: ['./projects-list.component.scss'],
 })
 export class ProjectsListComponent implements OnInit {
+  projects: Project[] = [];
 
-  constructor() { }
+  constructor(private projectsService: ProjectService) {}
 
-  ngOnInit(): void {
+  get isEmpty() {
+    return !this.projects.length;
   }
 
+  ngOnInit(): void {
+    this.projects = this.projectsService.getAll();
+  }
 }
