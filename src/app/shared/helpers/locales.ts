00d0beea2e0169efeaff3e9f095bf200f7babfbd
Anderson Feitosa <andersonmfjr@gmail.com>
2022-10-15 15:25:17 -0300

    feat(project): add more locales options

--- src/app/shared/helpers/locales.ts
+++ src/app/shared/helpers/locales.ts
@@ -1,14 +1,650 @@
 export const LOCALES = [
   {
-    label: 'English (United States)',
+    value: 'af-ZA',
+    label: 'Afrikaans (South Africa)',
+  },
+  {
+    value: 'ar-AE',
+    label: 'Arabic (U.A.E.)',
+  },
+  {
+    value: 'ar-BH',
+    label: 'Arabic (Bahrain)',
+  },
+  {
+    value: 'ar-DZ',
+    label: 'Arabic (Algeria)',
+  },
+  {
+    value: 'ar-EG',
+    label: 'Arabic (Egypt)',
+  },
+  {
+    value: 'ar-IQ',
+    label: 'Arabic (Iraq)',
+  },
+  {
+    value: 'ar-JO',
+    label: 'Arabic (Jordan)',
+  },
+  {
+    value: 'ar-KW',
+    label: 'Arabic (Kuwait)',
+  },
+  {
+    value: 'ar-LB',
+    label: 'Arabic (Lebanon)',
+  },
+  {
+    value: 'ar-LY',
+    label: 'Arabic (Libya)',
+  },
+  {
+    value: 'ar-MA',
+    label: 'Arabic (Morocco)',
+  },
+  {
+    value: 'ar-OM',
+    label: 'Arabic (Oman)',
+  },
+  {
+    value: 'ar-QA',
+    label: 'Arabic (Qatar)',
+  },
+  {
+    value: 'ar-SA',
+    label: 'Arabic (Saudi Arabia)',
+  },
+  {
+    value: 'ar-SY',
+    label: 'Arabic (Syria)',
+  },
+  {
+    value: 'ar-TN',
+    label: 'Arabic (Tunisia)',
+  },
+  {
+    value: 'ar-YE',
+    label: 'Arabic (Yemen)',
+  },
+  {
+    value: 'az-AZ',
+    label: 'Azeri (Latin) (Azerbaijan)',
+  },
+  {
+    value: 'az-AZ',
+    label: 'Azeri (Cyrillic) (Azerbaijan)',
+  },
+  {
+    value: 'be-BY',
+    label: 'Belarusian (Belarus)',
+  },
+  {
+    value: 'bg-BG',
+    label: 'Bulgarian (Bulgaria)',
+  },
+  {
+    value: 'bs-BA',
+    label: 'Bosnian (Bosnia and Herzegovina)',
+  },
+  {
+    value: 'ca-ES',
+    label: 'Catalan (Spain)',
+  },
+  {
+    value: 'cs-CZ',
+    label: 'Czech (Czech Republic)',
+  },
+  {
+    value: 'cy-GB',
+    label: 'Welsh (United Kingdom)',
+  },
+  {
+    value: 'da-DK',
+    label: 'Danish (Denmark)',
+  },
+  {
+    value: 'de-AT',
+    label: 'German (Austria)',
+  },
+  {
+    value: 'de-CH',
+    label: 'German (Switzerland)',
+  },
+  {
+    value: 'de-DE',
+    label: 'German (Germany)',
+  },
+  {
+    value: 'de-LI',
+    label: 'German (Liechtenstein)',
+  },
+  {
+    value: 'de-LU',
+    label: 'German (Luxembourg)',
+  },
+  {
+    value: 'dv-MV',
+    label: 'Divehi (Maldives)',
+  },
+  {
+    value: 'el-GR',
+    label: 'Greek (Greece)',
+  },
+  {
+    value: 'en-AU',
+    label: 'English (Australia)',
+  },
+  {
+    value: 'en-BZ',
+    label: 'English (Belize)',
+  },
+  {
+    value: 'en-CA',
+    label: 'English (Canada)',
+  },
+  {
+    value: 'en-CB',
+    label: 'English (Caribbean)',
+  },
+  {
+    value: 'en-GB',
+    label: 'English (United Kingdom)',
+  },
+  {
+    value: 'en-IE',
+    label: 'English (Ireland)',
+  },
+  {
+    value: 'en-JM',
+    label: 'English (Jamaica)',
+  },
+  {
+    value: 'en-NZ',
+    label: 'English (New Zealand)',
+  },
+  {
+    value: 'en-PH',
+    label: 'English (Republic of the Philippines)',
+  },
+  {
+    value: 'en-TT',
+    label: 'English (Trinidad and Tobago)',
+  },
+  {
     value: 'en-US',
+    label: 'English (United States)',
   },
   {
-    label: 'Portuguese (Brazil)',
-    value: 'pt-BR',
+    value: 'en-ZA',
+    label: 'English (South Africa)',
   },
   {
-    label: 'Spanish (Spain)',
+    value: 'en-ZW',
+    label: 'English (Zimbabwe)',
+  },
+  {
+    value: 'es-AR',
+    label: 'Spanish (Argentina)',
+  },
+  {
+    value: 'es-BO',
+    label: 'Spanish (Bolivia)',
+  },
+  {
+    value: 'es-CL',
+    label: 'Spanish (Chile)',
+  },
+  {
+    value: 'es-CO',
+    label: 'Spanish (Colombia)',
+  },
+  {
+    value: 'es-CR',
+    label: 'Spanish (Costa Rica)',
+  },
+  {
+    value: 'es-DO',
+    label: 'Spanish (Dominican Republic)',
+  },
+  {
+    value: 'es-EC',
+    label: 'Spanish (Ecuador)',
+  },
+  {
     value: 'es-ES',
+    label: 'Spanish (Castilian)',
+  },
+  {
+    value: 'es-ES',
+    label: 'Spanish (Spain)',
+  },
+  {
+    value: 'es-GT',
+    label: 'Spanish (Guatemala)',
+  },
+  {
+    value: 'es-HN',
+    label: 'Spanish (Honduras)',
+  },
+  {
+    value: 'es-MX',
+    label: 'Spanish (Mexico)',
+  },
+  {
+    value: 'es-NI',
+    label: 'Spanish (Nicaragua)',
+  },
+  {
+    value: 'es-PA',
+    label: 'Spanish (Panama)',
+  },
+  {
+    value: 'es-PE',
+    label: 'Spanish (Peru)',
+  },
+  {
+    value: 'es-PR',
+    label: 'Spanish (Puerto Rico)',
+  },
+  {
+    value: 'es-PY',
+    label: 'Spanish (Paraguay)',
+  },
+  {
+    value: 'es-SV',
+    label: 'Spanish (El Salvador)',
+  },
+  {
+    value: 'es-UY',
+    label: 'Spanish (Uruguay)',
+  },
+  {
+    value: 'es-VE',
+    label: 'Spanish (Venezuela)',
+  },
+  {
+    value: 'et-EE',
+    label: 'Estonian (Estonia)',
+  },
+  {
+    value: 'eu-ES',
+    label: 'Basque (Spain)',
+  },
+  {
+    value: 'fa-IR',
+    label: 'Farsi (Iran)',
+  },
+  {
+    value: 'fi-FI',
+    label: 'Finnish (Finland)',
+  },
+  {
+    value: 'fo-FO',
+    label: 'Faroese (Faroe Islands)',
+  },
+  {
+    value: 'fr-BE',
+    label: 'French (Belgium)',
+  },
+  {
+    value: 'fr-CA',
+    label: 'French (Canada)',
+  },
+  {
+    value: 'fr-CH',
+    label: 'French (Switzerland)',
+  },
+  {
+    value: 'fr-FR',
+    label: 'French (France)',
+  },
+  {
+    value: 'fr-LU',
+    label: 'French (Luxembourg)',
+  },
+  {
+    value: 'fr-MC',
+    label: 'French (Principality of Monaco)',
+  },
+  {
+    value: 'gl-ES',
+    label: 'Galician (Spain)',
+  },
+  {
+    value: 'gu-IN',
+    label: 'Gujarati (India)',
+  },
+  {
+    value: 'he-IL',
+    label: 'Hebrew (Israel)',
+  },
+  {
+    value: 'hi-IN',
+    label: 'Hindi (India)',
+  },
+  {
+    value: 'hr-BA',
+    label: 'Croatian (Bosnia and Herzegovina)',
+  },
+  {
+    value: 'hr-HR',
+    label: 'Croatian (Croatia)',
+  },
+  {
+    value: 'hu-HU',
+    label: 'Hungarian (Hungary)',
+  },
+  {
+    value: 'hy-AM',
+    label: 'Armenian (Armenia)',
+  },
+  {
+    value: 'id-ID',
+    label: 'Indonesian (Indonesia)',
+  },
+  {
+    value: 'is-IS',
+    label: 'Icelandic (Iceland)',
+  },
+  {
+    value: 'it-CH',
+    label: 'Italian (Switzerland)',
+  },
+  {
+    value: 'it-IT',
+    label: 'Italian (Italy)',
+  },
+  {
+    value: 'ja-JP',
+    label: 'Japanese (Japan)',
+  },
+  {
+    value: 'ka-GE',
+    label: 'Georgian (Georgia)',
+  },
+  {
+    value: 'kk-KZ',
+    label: 'Kazakh (Kazakhstan)',
+  },
+  {
+    value: 'kn-IN',
+    label: 'Kannada (India)',
+  },
+  {
+    value: 'ko-KR',
+    label: 'Korean (Korea)',
+  },
+  {
+    value: 'kok',
+    label: 'Konkani',
+  },
+  {
+    value: 'kok-IN',
+    label: 'Konkani (India)',
+  },
+  {
+    value: 'ky-KG',
+    label: 'Kyrgyz (Kyrgyzstan)',
+  },
+  {
+    value: 'lt-LT',
+    label: 'Lithuanian (Lithuania)',
+  },
+  {
+    value: 'lv-LV',
+    label: 'Latvian (Latvia)',
+  },
+  {
+    value: 'mi-NZ',
+    label: 'Maori (New Zealand)',
+  },
+  {
+    value: 'mk-MK',
+    label: 'FYRO Macedonian (Former Yugoslav Republic of Macedonia)',
+  },
+  {
+    value: 'mn-MN',
+    label: 'Mongolian (Mongolia)',
+  },
+  {
+    value: 'mr-IN',
+    label: 'Marathi (India)',
+  },
+  {
+    value: 'ms-BN',
+    label: 'Malay (Brunei Darussalam)',
+  },
+  {
+    value: 'ms-MY',
+    label: 'Malay (Malaysia)',
+  },
+  {
+    value: 'mt-MT',
+    label: 'Maltese (Malta)',
+  },
+  {
+    value: 'nb-NO',
+    label: 'Norwegian (Bokm?l) (Norway)',
+  },
+  {
+    value: 'nl-BE',
+    label: 'Dutch (Belgium)',
+  },
+  {
+    value: 'nl-NL',
+    label: 'Dutch (Netherlands)',
+  },
+  {
+    value: 'nn-NO',
+    label: 'Norwegian (Nynorsk) (Norway)',
+  },
+  {
+    value: 'ns-ZA',
+    label: 'Northern Sotho (South Africa)',
+  },
+  {
+    value: 'pa-IN',
+    label: 'Punjabi (India)',
+  },
+  {
+    value: 'pl-PL',
+    label: 'Polish (Poland)',
+  },
+  {
+    value: 'ps-AR',
+    label: 'Pashto (Afghanistan)',
+  },
+  {
+    value: 'pt-BR',
+    label: 'Portuguese (Brazil)',
+  },
+  {
+    value: 'pt-PT',
+    label: 'Portuguese (Portugal)',
+  },
+  {
+    value: 'qu-BO',
+    label: 'Quechua (Bolivia)',
+  },
+  {
+    value: 'qu-EC',
+    label: 'Quechua (Ecuador)',
+  },
+  {
+    value: 'qu-PE',
+    label: 'Quechua (Peru)',
+  },
+  {
+    value: 'ro-RO',
+    label: 'Romanian (Romania)',
+  },
+  {
+    value: 'ru-RU',
+    label: 'Russian (Russia)',
+  },
+  {
+    value: 'sa-IN',
+    label: 'Sanskrit (India)',
+  },
+  {
+    value: 'se-FI',
+    label: 'Sami (Northern) (Finland)',
+  },
+  {
+    value: 'se-FI',
+    label: 'Sami (Skolt) (Finland)',
+  },
+  {
+    value: 'se-FI',
+    label: 'Sami (Inari) (Finland)',
+  },
+  {
+    value: 'se-NO',
+    label: 'Sami (Northern) (Norway)',
+  },
+  {
+    value: 'se-NO',
+    label: 'Sami (Lule) (Norway)',
+  },
+  {
+    value: 'se-NO',
+    label: 'Sami (Southern) (Norway)',
+  },
+  {
+    value: 'se-SE',
+    label: 'Sami (Northern) (Sweden)',
+  },
+  {
+    value: 'se-SE',
+    label: 'Sami (Lule) (Sweden)',
+  },
+  {
+    value: 'se-SE',
+    label: 'Sami (Southern) (Sweden)',
+  },
+  {
+    value: 'sk-SK',
+    label: 'Slovak (Slovakia)',
+  },
+  {
+    value: 'sl-SI',
+    label: 'Slovenian (Slovenia)',
+  },
+  {
+    value: 'sq-AL',
+    label: 'Albanian (Albania)',
+  },
+  {
+    value: 'sr-BA',
+    label: 'Serbian (Latin) (Bosnia and Herzegovina)',
+  },
+  {
+    value: 'sr-BA',
+    label: 'Serbian (Cyrillic) (Bosnia and Herzegovina)',
+  },
+  {
+    value: 'sr-SP',
+    label: 'Serbian (Latin) (Serbia and Montenegro)',
+  },
+  {
+    value: 'sr-SP',
+    label: 'Serbian (Cyrillic) (Serbia and Montenegro)',
+  },
+  {
+    value: 'sv-FI',
+    label: 'Swedish (Finland)',
+  },
+  {
+    value: 'sv-SE',
+    label: 'Swedish (Sweden)',
+  },
+  {
+    value: 'sw-KE',
+    label: 'Swahili (Kenya)',
+  },
+  {
+    value: 'syr',
+    label: 'Syriac',
+  },
+  {
+    value: 'syr-SY',
+    label: 'Syriac (Syria)',
+  },
+  {
+    value: 'ta-IN',
+    label: 'Tamil (India)',
+  },
+  {
+    value: 'te-IN',
+    label: 'Telugu (India)',
+  },
+  {
+    value: 'th-TH',
+    label: 'Thai (Thailand)',
+  },
+  {
+    value: 'tl-PH',
+    label: 'Tagalog (Philippines)',
+  },
+  {
+    value: 'tn-ZA',
+    label: 'Tswana (South Africa)',
+  },
+  {
+    value: 'tr-TR',
+    label: 'Turkish (Turkey)',
+  },
+  {
+    value: 'tt-RU',
+    label: 'Tatar (Russia)',
+  },
+  {
+    value: 'uk-UA',
+    label: 'Ukrainian (Ukraine)',
+  },
+  {
+    value: 'ur-PK',
+    label: 'Urdu (Islamic Republic of Pakistan)',
+  },
+  {
+    value: 'uz-UZ',
+    label: 'Uzbek (Latin) (Uzbekistan)',
+  },
+  {
+    value: 'uz-UZ',
+    label: 'Uzbek (Cyrillic) (Uzbekistan)',
+  },
+  {
+    value: 'vi-VN',
+    label: 'Vietnamese (Viet Nam)',
+  },
+  {
+    value: 'xh-ZA',
+    label: 'Xhosa (South Africa)',
+  },
+  {
+    value: 'zh-CN',
+    label: 'Chinese (S)',
+  },
+  {
+    value: 'zh-HK',
+    label: 'Chinese (Hong Kong)',
+  },
+  {
+    value: 'zh-MO',
+    label: 'Chinese (Macau)',
+  },
+  {
+    value: 'zh-SG',
+    label: 'Chinese (Singapore)',
+  },
+  {
+    value: 'zh-TW',
+    label: 'Chinese (T)',
+  },
+  {
+    value: 'zu-ZA',
+    label: 'Zulu (South Africa)',
   },
 ];
